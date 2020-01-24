ced0cc2293fd0bb7ea2c090fd5c2b6d35288018e
Tracy Tran <tran-tracy@hotmail.com>
2020-01-24 09:08:03 -0500

    commit 1

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* Problem A: Generate a random dice number between 1-6*/
+
+/* Problem B: Modify the dicemsg <output> to show the roll number
+    1. Find the .dicemsg element in the document
+    2. Change the contents to show the number (You rolled: X)*/
+
+/* Problem C: Modify the diceface <img> to show a new dice face 
+    1. Find the .diceface element in the document
+    2. Change the "src" attribute to an image with the number of the roll*/
+
+/* Problem D: Make the button clickable to run Problems A-C */
+
+// function diceRolltoString(){
+//     let diceRoll = Math.floor(Math.random() * 6 + 1)
+//     return diceRoll
+// }
+
+// let diceRollString = `You rolled: ${diceRollString()}`
+
+// let diceRollImage =`img/dice${diceRollString()}.svg`
+
+// let btnEle = document.getElementById(rollBtn)
+// let diceLbl = document.getElementById(diceRollLabel)
+// let diceImg = document.getElementById(diceRollImage)
+
+// btnEle.addEventListener(`click`, event => {
+
+// })
